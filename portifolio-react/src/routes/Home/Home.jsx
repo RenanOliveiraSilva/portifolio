c41f4e97c915394fed23a960476d7a6d7369cbf3
Renan de Oliveira Silva <89980308+RenanOliveiraSilva@users.noreply.github.com>
2024-07-12 19:43:47 +0000

    Desenv

--- portifolio-react/src/routes/Home/Home.jsx
+++ portifolio-react/src/routes/Home/Home.jsx
@@ -2,15 +2,20 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
+import Badge from 'react-bootstrap/Badge';
 
 function Home() {
   return (
-    <Container fluid="md">
+    <Container fluid="md" className='p-2'>
       <Row>
         <Col>
           <Image src="holder.js/171x180" roundedCircle />
         </Col>
-        <Col>bbbbbb</Col>
+        <Col>
+          <Badge pill bg="light" text="dark">
+            Olá, que bom te conhecer
+          </Badge>
+        </Col>
       </Row>
     </Container>
   );
