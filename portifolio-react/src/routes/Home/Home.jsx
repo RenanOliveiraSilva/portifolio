c41059150cf0ff9148423702ea741853c515f1fb
Renan de Oliveira Silva <89980308+RenanOliveiraSilva@users.noreply.github.com>
2024-07-15 16:14:59 +0000

    Desenv

--- portifolio-react/src/routes/Home/Home.jsx
+++ portifolio-react/src/routes/Home/Home.jsx
@@ -3,13 +3,22 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
 import Badge from 'react-bootstrap/Badge';
+import avatar from '../../assets/013.jpg'
 
 function Home() {
   return (
-    <Container fluid="md" className='p-2'>
+    <Container fluid="md" className='p-2 pt-4 mt-5'>
       <Row>
-        <Col>
-          <Image src="holder.js/171x180" roundedCircle />
+        <Col className='d-flex' style={{alignItems: 'center', justifyContent: 'center' }}>
+          <Image
+            src={avatar}
+            style={{
+              width: '350px', 
+              height: '350px',
+              
+            }}
+            roundedCircle 
+          />
         </Col>
         <Col>
           <Badge pill bg="light" text="dark">
