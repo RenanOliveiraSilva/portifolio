050e105f0af2be8f18f0f9df0a88247fc4ff274d
Renan de Oliveira Silva <89980308+RenanOliveiraSilva@users.noreply.github.com>
2024-07-15 16:59:24 +0000

    Desenv

--- portifolio-react/src/routes/Home/Home.jsx
+++ portifolio-react/src/routes/Home/Home.jsx
@@ -3,21 +3,33 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
 import Badge from 'react-bootstrap/Badge';
-import avatar from '../../assets/013.jpg'
+import avatar from '../../assets/b.png'
+import teste from '../../assets/a.gif'
+import './Home.css'
 
 function Home() {
   return (
     <Container fluid="md" className='p-2 pt-4 mt-5'>
       <Row>
-        <Col className='d-flex' style={{alignItems: 'center', justifyContent: 'center' }}>
+      <Col className='d-flex justify-content-center position-relative'>
+          <Image
+            src={teste}
+            style={{ width: '350px', height: '350px' }}
+            roundedCircle
+          />
           <Image
             src={avatar}
+            className="overlay-image" // Classe para o hover
             style={{
-              width: '350px', 
+              width: '350px',
               height: '350px',
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
               
+              transition: 'transform 0.3s' // Transição suave
             }}
-            roundedCircle 
           />
         </Col>
         <Col>
