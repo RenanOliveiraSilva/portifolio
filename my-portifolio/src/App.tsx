8e74bf4d34eed028666a188e783aa4940c826ed6
RenanOliveiraSilva <renandeoliveiraisilva123@gmail.com>
2024-08-26 16:17:39 -0300

    Desenv

--- my-portifolio/src/App.tsx
+++ my-portifolio/src/App.tsx
@@ -1,16 +1,17 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import Home from "./screens/Home/Home"
-
 export default function App() {
-
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <Home />
-    }
-  ])
-
   return (
-    <RouterProvider router={router} />
+    <div className="flex justify-center items-center bg-[url('/Background_Intro.png')] bg-no-repeat bg-cover bg-center h-full">
+      <div className="h-[447px] w-[1040px] flex justify-center items-center">
+        <div className="h-[327px] w-[680px] flex justify-center items-center">
+          <div className="avatar">
+            <img src="https://img.freepik.com/vetores-premium/ilustracao-de-avatar-de-estudante-icone-de-perfil-de-usuario-avatar-de-jovem_118339-4402.jpg" alt="avatar" />
+          </div>
+          <div className="a">TEste</div>
+        </div>
+      </div>
+
+      
+      
+    </div>
   )
 }
--- my-portifolio/tailwind.config.js
+++ my-portifolio/tailwind.config.js
@@ -1,27 +1,19 @@
-const flowbite = require("flowbite-react/tailwind");
-
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    'node_modules/flowbite-react/lib/esm/**/*.js',
-    flowbite.content(),
   ],
   theme: {
     extend: {
       colors: {
-        customBlue: '#2b2d42', // Cor anterior
-        coolGray: '#8d99ae',
-        paleSky: '#edf2f4',
-        crimsonRed: '#ef233c',
-        darkRed: '#d90429',
-        white: '#FEFEFE'
+        customRed: '#E3646E', // Cor anterior
+        customPurple: '#BB72E9',
+        customBlue: '#3996DB',
+        customGreen: '#82BC4F',
+        customYellow: '#EABD5F',
       }
     },
   },
-  plugins: [
-    require('flowbite/plugin')
-  ]
+  plugins: [],
 }
