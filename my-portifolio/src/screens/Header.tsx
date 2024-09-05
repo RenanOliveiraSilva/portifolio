7a91104a5d0ca036aea0cdb48f6011157f2fc7ed
Renan de Oliveira Silva <renan-oliveira@altamogiana.com.br>
2024-09-05 12:55:30 -0300

    Ajustando Responsividade

--- my-portifolio/src/screens/Header.tsx
+++ my-portifolio/src/screens/Header.tsx
@@ -1,4 +1,5 @@
 import { FaGithub, FaHtml5, FaCss3Alt, FaJsSquare, FaReact, FaNodeJs, FaDatabase, FaArrowDown } from 'react-icons/fa';
+import { SiMongodb, SiAndroidstudio  } from "react-icons/si";
 
 const Header = () => {
   return (
@@ -16,20 +17,23 @@ const Header = () => {
 
         {/* Conteúdo textual */}
         <div className="teste py-5 h-[400px] flex flex-col justify-start mb-2">
-          <div className="content flex flex-col items-center justify-center space-y-3 max-w-3xl text-center mb-5">
-            <p className="font-inconsolata text-xs md:text-3xl font-light">
-              Hello World! Meu nome é <span className="text-customPurple font-bold">Renan Silva</span> e eu sou
-            </p>
-            <h2 className="text-3xl md:text-6xl font-bold font-asap mb-6">
-              Desenvolvedor <span className="block md:inline">Front-End</span>
-            </h2>
+          <div className="content flex flex-col items-center justify-center space-y-2 max-w-3xl text-center mb-5">
+            <div className="title">
+              <p className="font-inconsolata text-xs md:text-3xl font-light mb-1">
+                Hello World! Meu nome é <span className="text-customPurple font-bold">Renan Silva</span> e eu sou
+              </p>
+              <h2 className="text-3xl md:text-6xl font-bold font-asap mb-6">
+                Desenvolvedor <span className="block md:inline">Front-End</span>
+              </h2>
+
+            </div>
             <p className="text-sm md:text-base font-light font-maven text-gray-500">
               Minha paixão pela tecnologia impulsionou minha trajetória até aqui, sempre buscando novos desafios e aprendizados. Estou em constante busca por oportunidades que me permitam crescer e evoluir como profissional, contribuindo com soluções inovadoras e impactantes.
             </p>
           </div>
 
           {/* Ícones de tecnologia */}
-          <div className="flex mt-6 space-x-4 max-w-3xl justify-center flex-wrap gap-2">
+          <div className="flex mt-6 space-x-3 max-w-3xl justify-center flex-wrap gap-2">
             {[
               { icon: FaGithub, label: "GitHub", color: "text-gray-300", animation: "animate-pulse" },
               { icon: FaHtml5, label: "HTML", color: "text-customRed", animation: "animate-pulse" },
@@ -38,6 +42,8 @@ const Header = () => {
               { icon: FaReact, label: "ReactJs", color: "text-customBlue", animation: "animate-spin-slow" },
               { icon: FaNodeJs, label: "NodeJs", color: "text-customGreen", animation: "animate-pulse" },
               { icon: FaDatabase, label: "SQL", color: "text-customPurple", animation: "animate-pulse" },
+              { icon: SiMongodb, label: "MongoDB", color: "text-customGreen", animation: "animate-pulse" },
+              { icon: SiAndroidstudio, label: "Mobile", color: "text-customGray", animation: "animate-pulse" },
             ].map(({ icon: Icon, label, color, animation }, idx) => (
               <div key={idx} className="flex items-center space-x-2 px-3 py-1 md:py-2 md:px-4 bg-gray-700 rounded-full text-gray-300 shadow-lg">
                 <Icon className={`w-5 h-5 ${color} ${animation}`} />
