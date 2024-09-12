2866f509e007018cc79fecc360ec9f6a08bc3e81
Renan de Oliveira Silva <renan-oliveira@altamogiana.com.br>
2024-09-12 10:48:08 -0300

    Alterando Cards

--- my-portifolio/src/components/Card.tsx
+++ my-portifolio/src/components/Card.tsx
@@ -5,26 +5,32 @@ interface CardProps {
 }
 
 
-export default function Card( {title, description, image}: CardProps ) {
+export default function Card( ) {
     return (
-        <div className="bg-gray-900 text-white p-5 rounded-lg shadow-lg max-w-sm hover:animate-pulse">
-          {/* Card image */}
-          <div className="bg-pink-200 px-4 pt-4 rounded-lg">
-            <img
-              src={image} // Substitua pelo caminho da sua imagem
-              alt="Travelgram mockup"
+        /* From Uiverse.io by Javierrocadev */ 
+<div
+  className="group flex flex-col justify-start items-start gap-2 w-96 h-56 duration-500 relative rounded-lg p-4 bg-gray-100 hover:-translate-y-2 hover:shadow-xl shadow-gray-300"
+>
+  <div
+    className="absolute duration-700 shadow-md group-hover:-translate-y-4 group-hover:-translate-x-4 -bottom-10 -right-10 w-1/2 h-1/2 rounded-lg bg-gray-200"
+    
+  ></div>
 
-            />
-          </div>
+  <div className="">
+    <h2 className="text-2xl font-bold mb-2 text-gray-800">Card</h2>
+    <p className="text-gray-700 line-clamp-3">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean convallis
+      magna quis lectus fermentum, quis scelerisque orci pellentesque. Duis id
+      porta justo. Sed ac enim id justo tincidunt hendrerit id ac lectus.
+      Pellentesque maximus posuere tortor vitae consequat.
+    </p>
+  </div>
+  <button
+    className="hover:bg-gray-300 bg-gray-200 text-gray-800 mt-6 rounded p-2 px-6"
+  >
+    Explore
+  </button>
+</div>
 
-          {/* Card content */}
-            <div className="mt-5">
-                <h2 className="text-2xl font-bold">{title}</h2>
-                <p className="text-gray-400 mt-2">
-                    {description}	
-                </p>
-            </div>
-      
-        </div>
     );
 }
--- my-portifolio/src/screens/Works.tsx
+++ my-portifolio/src/screens/Works.tsx
@@ -1,25 +1,24 @@
-import Card from '../components/Card'
+import Card from '../components/Card';
 
 const Works = () => {
   return (
     <section className='flex flex-col h-fit bg-backgroundColor px-6 py-2 justify-center'>
-        <div className="title flex flex-col items-center justify-center px-5 py-1">
-            <p className='font-inconsolata text-regular md:text-2xl text-customRed font-light'>Meu trabalho</p>
-            <p className='font-inconsolata text-xl md:text-4xl font-bold '>Projetos desenvolvidos por mim</p>
-        </div>
-        <div className="cards px-2 py-1 md:px-20  mt-5 flex flex-row gap-5 flex-wrap justify-between">
-          <Card title='List-ToDo React' description='Projeto web utilizando ReactTSX e MongoDB' image='/Background_Intro.png'/>
-          <Card title='List-ToDo React' description='Projeto web utilizando ReactTSX e MongoDB' image='/Background_Intro.png'/>
-          <Card title='List-ToDo React' description='Projeto web utilizando ReactTSX e MongoDB' image='/Background_Intro.png'/>
-          <Card title='List-ToDo React' description='Projeto web utilizando ReactTSX e MongoDB' image='/Background_Intro.png'/>
-          <Card title='List-ToDo React' description='Projeto web utilizando ReactTSX e MongoDB' image='/Background_Intro.png'/>
-          <Card title='List-ToDo React' description='Projeto web utilizando ReactTSX e MongoDB' image='/Background_Intro.png'/>
+      <div className="title flex flex-col items-center justify-center px-5 py-1">
+        <p className='font-inconsolata text-regular md:text-2xl text-customRed font-light'>Meu trabalho</p>
+        <p className='font-inconsolata text-xl md:text-4xl font-bold'>Projetos desenvolvidos por mim</p>
+      </div>
 
-        </div>
-        
+      <div className="cards px-2 py-1 md:px-20 h-fit mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-14 justify-between">
+        <Card />
+        <Card />
+        <Card />
 
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Works
+export default Works;
