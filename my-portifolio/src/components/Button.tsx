bfaebb5577972f98a72131bce9969dabe9f25ee6
Renan de Oliveira Silva <renan-oliveira@altamogiana.com.br>
2024-09-19 16:46:59 -0300

    Atualizando about

--- my-portifolio/src/components/Button.tsx
+++ my-portifolio/src/components/Button.tsx
@@ -4,8 +4,17 @@ interface ButtonProps {
 
 export default function Button({title}: ButtonProps) {
   return (
-    <button className="group font-asap border-none group-hover:before:duration-500 group-hover:after:duration-500 after:duration-500 hover:border-customPurple hover:before:[box-shadow:_20px_20px_20px_30px_#a21caf] duration-500 before:duration-500 hover:duration-500 underline underline-offset-2 hover:after:-right-8 hover:before:right-12 hover:before:-bottom-8 hover:before:blur hover:underline hover:underline-offset-4  origin-left hover:decoration-2 hover:text-customPurple relative bg-gray-900 h-12 md:h-16 w-32 md:w-56 border text-left p-3 text-gray-50 text-sm font-semibold rounded-lg  overflow-hidden  before:absolute before:w-12 before:h-12 before:content[''] before:right-1 before:top-1 before:z-10 before:bg-purple-200 before:rounded-full before:blur-lg  after:absolute after:z-10 after:w-20 after:h-20 after:content['']  after:bg-customPurple after:right-8 after:top-3 after:rounded-full after:blur-lg">
-      {title}
+    <button className="relative flex w-4/5 items-center px-6 py-3 overflow-hidden font-medium transition-all bg-customPurple rounded-md group">
+      <span className="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-purple-600 rounded group-hover:-mr-4 group-hover:-mt-4">
+        <span className="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white" />
+      </span>
+      <span className="absolute bottom-0 rotate-180 left-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-purple-600 rounded group-hover:-ml-4 group-hover:-mb-4">
+        <span className="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white" />
+      </span>
+      <span className="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full bg-purple-600 rounded-md group-hover:translate-x-0" />
+      <span className="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-white">
+        {title}
+      </span>
     </button>
   );
 };
